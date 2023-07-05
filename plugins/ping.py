fbeb7bbb87345d240814f7658e73498faa2b4082
ZIYAN <92292759+DX-MODS@users.noreply.github.com>
2023-07-05 21:56:23 +0530

    Create ping.py

---
+++ plugins/ping.py
@@ -0,0 +1,8 @@
+from pyrogram import Client,filters
+@DxStreamBot.on_message(filters.regex("ping📡"))
+async def ping(b, m):
+    start_t = time.time()
+    ag = await m.reply_text("....")
+    end_t = time.time()
+    time_taken_s = (end_t - start_t) * 1000
+    await ag.edit(f"Pong!\n{time_taken_s:.3f} ms")
