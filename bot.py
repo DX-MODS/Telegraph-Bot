2587dc05ce48d23493cea296e545392b7d564053
ZIYAN <92292759+DX-MODS@users.noreply.github.com>
2023-07-05 21:28:01 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ class Bot(Client):
 
     def __init__(self):
         super().__init__(
-            name="renamer",
+            name="Telegraph-Bot",
             api_id=Config.API_ID,
             api_hash=Config.API_HASH,
             bot_token=Config.BOT_TOKEN,
