b40f39595fe8707fc7a58d300d6af973897cf8d3
ZIYAN <92292759+DX-MODS@users.noreply.github.com>
2023-07-07 02:11:39 +0530

    Add files via upload

---
+++ plugins/ping.py
@@ -0,0 +1,10 @@
+from pyrogram import Client as DxMods,filters
+import time
+
+@DxMods.on_message(filters.regex("ping📡"))
+async def ping(b, m):
+    start_t = time.time()
+    ag = await m.reply_text("....")
+    end_t = time.time()
+    time_taken_s = (end_t - start_t) * 1000
+    await ag.edit(f"Pong!\n{time_taken_s:.3f} ms")
