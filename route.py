f99eea3bf01cc4cf0269e2884557004f71a6efb3
ZIYAN <92292759+DX-MODS@users.noreply.github.com>
2023-07-07 17:15:43 +0530

    Update route.py

--- route.py
+++ route.py
@@ -3,9 +3,30 @@ import time
 StartTime = time.time()
 __version__ = 1.1
 from helper.client import multi_clients, work_loads, DxTelegraphBot
-from helper.utils.readable_time import get_readable_time
-routes = web.RouteTableDef()
 
+routes = web.RouteTableDef()
+def get_readable_time(seconds: int) -> str:
+    count = 0
+    readable_time = ""
+    time_list = []
+    time_suffix_list = ["s", "m", "h", " days"]
+    while count < 4:
+        count += 1
+        if count < 3:
+            remainder, result = divmod(seconds, 60)
+        else:
+            remainder, result = divmod(seconds, 24)
+        if seconds == 0 and remainder == 0:
+            break
+        time_list.append(int(result))
+        seconds = int(remainder)
+    for x in range(len(time_list)):
+        time_list[x] = str(time_list[x]) + time_suffix_list[x]
+    if len(time_list) == 4:
+        readable_time += time_list.pop() + ", "
+    time_list.reverse()
+    readable_time += ": ".join(time_list)
+    return readable_time 
 @routes.get("/", allow_head=True)
 async def root_route_handler(_):
     return web.json_response(
