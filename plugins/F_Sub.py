548cb8b652d7fc9d04202b7e682ca855fbc197d9
ZIYAN <92292759+DX-MODS@users.noreply.github.com>
2023-07-09 10:32:41 +0530

    Update F_Sub.py

--- plugins/F_Sub.py
+++ plugins/F_Sub.py
@@ -7,9 +7,22 @@ from pyrogram import Client, filters, enums
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from pyrogram.errors import UserNotParticipant
 from config import Config
-from helper.database import not_subscribed
+from helper.database import db
 
-
+async def not_subscribed(_, client, message):
+    await db.add_user(client, message)
+    if not Config.FORCE_SUB:
+        return False
+    try:             
+        user = await client.get_chat_member(Config.FORCE_SUB, message.from_user.id) 
+        if user.status == enums.ChatMemberStatus.BANNED:
+            return True 
+        else:
+            return False                
+    except UserNotParticipant:
+        pass
+    return True
+    
 @Client.on_message(filters.private & filters.create(not_subscribed))
 async def forces_sub(client, message):
     buttons = [[InlineKeyboardButton(text="📢 Join Update Channel 📢", url=f"https://t.me/{Config.FORCE_SUB}") ]]
