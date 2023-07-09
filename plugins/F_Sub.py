985a8faa541f66ffddb317285f45b3b84c4be840
ZIYAN <92292759+DX-MODS@users.noreply.github.com>
2023-07-09 10:24:24 +0530

    Update F_Sub.py

--- plugins/F_Sub.py
+++ plugins/F_Sub.py
@@ -9,20 +9,6 @@ from pyrogram.errors import UserNotParticipant
 from config import Config
 from helper.database import db
 
-async def not_subscribed(_, client, message):
-    await db.add_user(client, message)
-    if not Config.FORCE_SUB:
-        return False
-    try:             
-        user = await client.get_chat_member(Config.FORCE_SUB, message.from_user.id) 
-        if user.status == enums.ChatMemberStatus.BANNED:
-            return True 
-        else:
-            return False                
-    except UserNotParticipant:
-        pass
-    return True
-
 
 @Client.on_message(filters.private & filters.create(not_subscribed))
 async def forces_sub(client, message):
@@ -36,4 +22,3 @@ async def forces_sub(client, message):
         return await message.reply_text(text=text, reply_markup=InlineKeyboardMarkup(buttons))
     return await message.reply_text(text=text, reply_markup=InlineKeyboardMarkup(buttons))
           
-
