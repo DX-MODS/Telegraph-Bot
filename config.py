2088efd48e864b582ea5d8e49144cf23e61c0525
ZIYAN <92292759+DX-MODS@users.noreply.github.com>
2023-07-17 14:26:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,24 +8,30 @@ import re, os, time
 id_pattern = re.compile(r'^.\d+$') 
 
 class Config(object):
-    # pyro client config
+    # pyrogram client config
     API_ID    = os.environ.get("API_ID", "")
     API_HASH  = os.environ.get("API_HASH", "")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
    
-    # database config
+    # database config get this from mongodb
     DB_NAME = os.environ.get("DB_NAME","Dxbotz")     
     DB_URL  = os.environ.get("DB_URL","")
  
     # other configs
     BOT_UPTIME  = time.time()
+    #start pic url this image will shown in start command get this from @DX_telegraphbot
     START_PIC   = os.environ.get("START_PIC", "")
-    ADMIN       = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '').split()]
+    ADMIN       = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '').split()
+    #the channel which need to force subscribed, channel username without @
     FORCE_SUB   = os.environ.get("FORCE_SUB", "") 
+    #the log channel id must start in -100 this channel will be were the bot send logs
     LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", None))
 
-    # wes response configuration     
+    # wes response configuration
+    #if your bot is web required give True or else False
     WEBHOOK = bool(os.environ.get("WEBHOOK", True))
     PORT = int(os.environ.get("PORT", "8080"))
-    PING_INTERVAL = int(os.environ.get("PING_INTERVAL", "120"))  
-    URL = os.environ.get("URL", "") 
+    #the interval time to ping server
+    PING_INTERVAL = int(os.environ.get("PING_INTERVAL", "120"))
+    #if your bot is running with web cmd pls copy the web link to ping server not down in 1 minutes
+    PING_WEB = os.environ.get("PING_WEB", "") 
